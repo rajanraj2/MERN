f89551a140ca4f636fe37b36129d58164e4beba9
BabyMonk <rans2203@gmail.com>
2024-04-12 17:58:45 +0530

    Add react in client

---
+++ TT_MERN/client/src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+export const Contact = () => {
+    return (
+        <div>
+        <h1>Contact Page</h1>
+        </div>
+    );
+}
+
+// export default Contact;
---
+++ TT_MERN/client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+export const Home = () => {
+    return (
+        <div>
+        <h1>Home Page</h1>
+        </div>
+    );
+    }
+
+// export default Home;
