225db13ce7c255be5502fa3d6fd02891185f615f
BabyMonk <rans2203@gmail.com>
2024-04-13 19:22:11 +0530

    Update about page with username

--- TT_MERN/client/src/pages/About.jsx
+++ TT_MERN/client/src/pages/About.jsx
@@ -1,7 +1,19 @@
+// import { useState } from "react";
+import { useAuth } from "../store/auth";
+
 export const About = () => {
+
+    // const [userData, setUserData] = useState(true);
+
+    const { user: currentUser } = useAuth();
+
+
     return (
         <div>
         <h1>About Page</h1>
+        <br/>
+        <h3>Welcome { currentUser ? `${currentUser.username} to our website` : `to our website`
+        }</h3>
         </div>
     );
     }
