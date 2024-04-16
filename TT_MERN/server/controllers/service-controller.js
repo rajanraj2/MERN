da0ef45681c606ea1f89afd52edabe8e294bc00c
Ruggy Mango <rans2203@gmail.com>
2024-04-16 23:02:40 +0530

    Send image data to database

--- TT_MERN/server/controllers/service-controller.js
+++ TT_MERN/server/controllers/service-controller.js
@@ -1,5 +1,7 @@
 // import Service from "../models/service-model";
 import { spawn } from 'child_process';
+import Image from "../models/image-model.js";
+
 
 
 const services = async (req, res) => {
@@ -44,13 +46,44 @@ const services = async (req, res) => {
         });
 
         // When Python process ends
-        pythonProcess.on('close', (code) => {
+        pythonProcess.on('close',async (code) => {
             // Send prediction result to client only once after Python process ends
-            res.status(200).json({msg: "File uploaded successfully", prediction: finalPredictionResult });
+            // res.status(200).json({ msg: "File uploaded successfully", prediction: finalPredictionResult });
             console.log(`Image successfully uploaded and recognised.`);
+            // console.log(response);
+            const imageData = {
+                imageName: filename,
+                clothType: finalPredictionResult,
+                email: "sample@gmail.com",
+                userId: "sample",
+                extra: "extra detail" // If "extra" field is also provided in the request body
+            };
+
+            await Image.create(imageData);
+            console.log(`Image data sent successfully :)`);
+            res.status(200).json({ message: "Image data sent successfully" });
         });
+
+        // console.log(response);
+        // try {
+        //     const imageData = {
+        //         imageName: req.body.filename,
+        //         clothType: finalPredictionResult,
+        //         email: "sample@gmail.com",
+        //         userId: "sample",
+        //         extra: "extra detail" // If "extra" field is also provided in the request body
+        //     };
+
+        //     await Image.create(imageData);
+        //     console.log(`Image data sent successfully :)`);
+        //     return res.status(200).json({ message: "Image data sent successfully" });
+            
+        // }
+        // catch (error) {
+        //     // return res.status(500).json({ message: "Image data not sent" });
+        // }
     }
-    catch(error) {
+    catch (error) {
         // console.log(`services: ${error}`);
         // res.status(500).send('Internal Server Error');
     }
